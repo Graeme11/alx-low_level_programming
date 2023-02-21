bdbf53909c05bbe5a25e7559b33af5d7a13c27ca
Graeme11 <122824984+Graeme11@users.noreply.github.com>
2023-02-21 15:36:34 +0200

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * add - add two integer
  * @i: The first integer to be added
  * @l: The Second integer to be added
- * return: the sum of the two 
+ * return: the sum of the two
  */
 int add(int i, int l)
 {
