ae2f31b72908678932041d55f379c27373a4cf78
Graeme11 <122824984+Graeme11@users.noreply.github.com>
2023-02-21 14:50:16 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+* print_alphabet - llamar la funcion desde el main
+*/
 void print_alphabet(void)
-{char letter = 'a';
-while (letter <= 'z') {
-putchar(letter);
-letter++;}
-putchar('\n');}
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
