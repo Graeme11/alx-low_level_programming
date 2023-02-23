f8b0111d9edd6c7be9d7ee1f9ff87848dbeeac74
Graeme11 <122824984+Graeme11@users.noreply.github.com>
2023-02-23 10:51:02 +0200

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - checks for lowercase character.
+ * @a: entire receipt for main.
+ * @b: integer received by main
+ * Return: 1 if c is uppercase
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
