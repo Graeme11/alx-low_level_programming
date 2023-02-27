716d97df89024dc578a8ea67a4f2c3dc4e2a870a
Graeme11 <122824984+Graeme11@users.noreply.github.com>
2023-02-27 14:35:59 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,11 @@
 #include "main.h"
- 
+
 /**
- * reset_to_98 - main function.
- * @n: Pointer with the address value.
- *
- * Description: This function takes a pointer to an int as parameter
- * and updates the value it points to to 98.
- *
- * Return: none.
+ *reset_to_98 - updates the value of integer
+ *@n: integer
+ *Return: nothing
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
